22aee415622187e1060771260f99f2ded3d14eb9
Simon Frost <sfrost007@gmail.com>
2015-02-10 11:27:11 +0000

    [tmux] Tailor work tmuxifier config for both machines

--- tmux/work.session.sh
+++ tmux/work.session.sh
@@ -1,6 +1,8 @@
 if initialize_session "Work"; then
   load_window "sdk-work"
-  load_window "ios-viewers"
+  if [[ `hostname -s` = spf-mac-d ]]; then
+    load_window "ios-viewers"
+  fi
   load_window "dotfiles-etc"
   select_window 1
 fi
