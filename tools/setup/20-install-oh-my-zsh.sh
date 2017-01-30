cd14d7de493bb6170ba657b3d0545424c50b8405
Simon Frost <sfrost007@gmail.com>
2017-01-30 19:32:19 +0000

    [tools] Tweak ZSH setup script to install required fonts and set ZSH as the shell

--- tools/setup/20-install-oh-my-zsh.sh
+++ tools/setup/20-install-oh-my-zsh.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+# Install Oh-my-zsh and clone any custom plugins/themes
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k
+
+# Install custom fonts required for Powerline
+cp -r ../fonts/* ~/Library/Fonts
+
+# Switch to zsh as the shell
+chsh -s /bin/zsh
