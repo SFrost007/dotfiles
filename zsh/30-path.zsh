04e625aa4538632b98efb0191ac1400b1338debc
Simon Frost <sfrost007@gmail.com>
2017-12-20 12:46:16 +0000

    [zsh] Change path to latest version of CMake

--- zsh/30-path.zsh
+++ zsh/30-path.zsh
@@ -12,6 +12,6 @@ function addPathDir() {
 # Work paths
 addPathDir $HOME/Code/WorkProjects/builds
 addPathDir $HOME/Code/WorkProjects/tools
-addPathDir /Applications/CMake_3.4.3.app/Contents/bin
+addPathDir /Applications/CMake_3.10.0.app/Contents/bin
 # My scripts
 addPathDir $DOTFILES_DIR/bin
