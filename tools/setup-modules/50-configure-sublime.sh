c42b83b3364e14806931fd8f99c668f619773307
Simon Frost <sfrost007@gmail.com>
2018-03-12 13:12:06 +0000

    [sublime] Move to auto-installer for config and Package Control

--- tools/setup-modules/50-configure-sublime.sh
+++ tools/setup-modules/50-configure-sublime.sh
@@ -9,35 +9,50 @@ _module_run_after_git_pull() {
 }
 
 _module_valid() {
-  # TODO: Check for presence of sublime packages dir and whether it's a symlink
-  return 1
+  if [[ "$(get_platform)" == "${PLATFORM_WSL}" ]]; then
+    return 1 # TODO: Support Windows appdata dir outside WSL
+  fi
+
+  # TODO: Remove this duplication
+  local SUBLIME_DIR
+  case `get_platform` in
+    $PLATFORM_OSX) SUBLIME_DIR="$HOME/Library/Application Support/Sublime Text 3";;
+    $PLATFORM_LINUX) SUBLIME_DIR="$HOME/.config/sublime-text-3";;
+  esac
+
+  if [[ -d "${SUBLIME_DIR}/Packages/User" ]]; then
+    return 1 # Already installed
+  fi
+
+  return 0
 }
 
 _module_exec() {
-  # TODO: Check over and enable this
-  echo "Ran configure-sublime"
-  #local overwrite_all=false backup_all=false skip_all=false
-  #$(./scripts/AssertOSX);
-  #if [[ $? -eq 0 ]]; then
-  #  SUBLIME_DIR=${HOME}/Library/Application\ Support/Sublime\ Text\ 2
-  #else
-  #  SUBLIME_DIR=${HOME}/.config/sublime-text-2
-  #fi
-  #
-  #mkdir -p "${SUBLIME_DIR}/Packages"
-  #link_file "${DOTFILES_ROOT}/sublime/" "${SUBLIME_DIR}/Packages/User"
-  #
-  #if [[ ! -f "${SUBLIME_DIR}/Installed Packages/Package Control.sublime-package" ]]; then
-  #  mkdir -p "${SUBLIME_DIR}/Installed Packages"
-  #  pushd "${SUBLIME_DIR}/Installed Packages" > /dev/null
-  #  wget http://sublime.wbond.net/Package%20Control.sublime-package -o /dev/null
-  #  if [[ $? -eq 0 ]]; then
-  #    success 'Installed Package Control for Sublime'
-  #  else
-  #    fail 'Failed to download Package Control'
-  #  fi
-  #  popd > /dev/null
-  #else
-  #  success 'Skipped installing Package Control - already exists'
-  #fi
+  local SUBLIME_DIR
+  case `get_platform` in
+    $PLATFORM_OSX) SUBLIME_DIR="$HOME/Library/Application Support/Sublime Text 3";;
+    $PLATFORM_LINUX) SUBLIME_DIR="$HOME/.config/sublime-text-3";;
+  esac
+
+  # Auto-install Package Control
+  INSTALLED_PKG_DIR="${SUBLIME_DIR}/Installed Packages"
+  if [[ ! -f "${INSTALLED_PKG_DIR}/Package Control.sublime-package" ]]; then
+    mkdir -p "${INSTALLED_PKG_DIR}"
+    pushd "${INSTALLED_PKG_DIR}" > /dev/null
+    wget http://sublime.wbond.net/Package%20Control.sublime-package -o /dev/null
+    if [[ $? -eq 0 ]]; then
+      success 'Installed Package Control for Sublime'
+    else
+      fail 'Failed to download Package Control'
+    fi
+    popd > /dev/null
+  else
+    success 'Skipped installing Package Control - already exists'
+  fi
+
+  # Link our dotfiles
+  PACKAGES_DIR="${SUBLIME_DIR}/Packages"
+  mkdir -p "${PACKAGES_DIR}"
+  ln -s "${HOME}/.dotfiles/sublime/" "${PACKAGES_DIR}/User"
+  success "Sublime config files configured"
 }
