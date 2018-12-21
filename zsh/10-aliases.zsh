8aa33d22abdf3c4c39586e0a6327259557d66448
Simon Frost <sfrost007@gmail.com>
2018-12-21 15:18:16 +0000

    [zsh] Add useful utilities and Android SDK paths

--- zsh/10-aliases.zsh
+++ zsh/10-aliases.zsh
@@ -6,15 +6,18 @@ alias du='du -h'
 alias cp='rsync --progress -ah'
 alias ccat='colorize'
 alias shutdown='sudo shutdown -h now'
+b64d() { echo "$1" | base64 -D }
 
 # Useful stuff
 alias myip='ifconfig | grep broadcast | awk -F " " '"'"'{print $2}'"'"
 alias sedrecurse='find . -type f -print | xargs sed -i ""'
 alias wakezion='wakeonlan 2c:76:8a:ab:d4:56'
+alias dotfiles='subl ~/.dotfiles'
 
 # Web-dev related stuff
 alias serve='python -m SimpleHTTPServer &'
 alias stopserve='pkill -f SimpleHTTPServer'
+alias prettyjson='python -m json.tool'
 
 # Source control
 alias gitcompress='git repack && git prune'
---
+++ zsh/50-android-sdk.zsh
@@ -0,0 +1,6 @@
+#!/bin/zsh
+
+export ANDROID_HOME="$HOME/Library/Android/sdk"
+addPathDir $ANDROID_HOME/platform-tools
+addPathDir $ANDROID_HOME/tools
+addPathDir $ANDROID_HOME/emulator
