e91dad176883c5c017f5ccf3fc2beb4d58cdb6b4
Simon Frost <sfrost007@gmail.com>
2021-01-27 19:46:14 +0000

    [git] Add diff-so-fancy as submodule for fewer dependencies

--- zsh/30-path.zsh
+++ zsh/30-path.zsh
@@ -15,5 +15,7 @@ addPathDir $HOME/Code/SDKs/flutter/bin
 addPathDir $GEM_HOME/bin
 # My scripts
 addPathDir $DOTFILES_DIR/bin
+# Git helper to avoid external dependency breaking `git diff`
+addPathDir $DOTFILES_DIR/git/diff-so-fancy
 # Machine-local (not git controlled) binaries
 addPathDir $HOME/.bin
