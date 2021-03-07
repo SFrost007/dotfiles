5dd42b583a10a127d70fa8f37ab3b1cc78f0e729
Simon Frost <sfrost007@gmail.com>
2021-03-07 10:35:34 +0000

    [setup] Add manual script for installing Flutter SDK

---
+++ dotfiles/install_flutter_sdk.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+export SDKS_DIR="${HOME}/Code/SDKs/"
+mkdir -p "${SDKS_DIR}"
+pushd "${SDKS_DIR}"
+
+git clone https://github.com/flutter/flutter.git -b dev
+
+FLUTTER_BIN="${SDKS_DIR}/flutter/bin/flutter"
+#"${FLUTTER_BIN}" channel dev # Only necessary if not checking out dev branch above
+"${FLUTTER_BIN}" upgrade
+"${FLUTTER_BIN}" config --enable-macos-desktop
+"${FLUTTER_BIN}" doctor
+
+echo "Done! Reload ZSH config to update path"
