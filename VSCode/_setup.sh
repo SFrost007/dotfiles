9a2e2867f7c232c9c0567efae0e0a2b6ed5a05bc
Simon Frost <sfrost007@gmail.com>
2025-12-17 15:34:16 +0000

    [vscode] Update installed extensions list and config

--- VSCode/_setup.sh
+++ VSCode/_setup.sh
@@ -17,25 +17,6 @@ ln -s "${DOTFILES_DIR}/VSCode/snippets/" "${VSCODE_USER_DIR}"
 echo ""
 
 # General
-code --install-extension CoenraadS.bracket-pair-colorizer-2
 code --install-extension eamodio.gitlens
-code --install-extension auchenberg.vscode-browser-preview
-code --install-extension hediet.vscode-drawio
+code --install-extension earshinov.filter-lines
 code --install-extension usernamehw.errorlens
-# Themes
-code --install-extension zhuangtongfa.material-theme
-# Javascript
-code --install-extension msjsdiag.vscode-react-native
-code --install-extension ronnidc.nunjucks
-# Dart
-code --install-extension Dart-Code.dart-code
-code --install-extension Dart-Code.flutter
-# Ruby
-code --install-extension bung87.rails
-code --install-extension bung87.vscode-gemfile
-code --install-extension rebornix.ruby
-code --install-extension sianglim.slim
-code --install-extension wingrunr21.vscode-ruby
-# Docker
-code --install-extension ms-azuretools.vscode-docker
-code --install-extension ms-vscode-remote.remote-containers
